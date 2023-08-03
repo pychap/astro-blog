f79600b1435545aa2b379b713876dd2cbcce05af
Peter Chapman <pchapman@ptc.com>
2023-08-03 15:52:32 -0700

    All integrations added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog.cip.netlify.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }), sitemap(), react(), tailwind(), partytown()]
 });
---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+// import { useState } from "react"
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(prev => prev + 1)}>{count}</button>
+  )
+}
+
+export default Counter
